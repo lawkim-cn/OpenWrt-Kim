93ba22b4aedabe13c94b33ea06d9b463ef238ba4
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2022-11-10 12:55:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.d
 
 # Add a feed source
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

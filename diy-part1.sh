577ff13ec5dc8bcc8d03dd469a93d3f1aab09471
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2022-11-10 13:18:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source

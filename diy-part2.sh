b80951ee32a000051a03a031664ca335c161a62e
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2022-12-16 09:45:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
 # Modify default Ver
-sed -i 's/R22.12.1/R22.12.1 Build By lawkim/g' package/lean/default/files/zzz-default-settings
+sed -i 's/R22.12.1/R22.12.1 Build By lawkim/g' package/lean/default-settings/files/zzz-default-settings
 # Modify default Themes
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/Makefile
 sed -i 's/bootstrap/argon/g' feeds/luci/collections/Makefile

f25043081434d12dc740ea08b045b91fe5c58e70
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2023-01-02 20:24:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
-echo 'src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+#echo 'src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

2de21330ee634d5a1542b794983c05a4025fe09a
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2024-08-28 16:51:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,12 +15,13 @@ sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_gener
 # Modify default Ver
 # sed -i 's/R23.7.7/R23.7.7 Build By LawKim/g' package/lean/default-settings/files/zzz-default-settings
 # Modify default Themes
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci-light/Makefile
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone -b 18.06 https://github.com/zxlhhyccc/luci-app-v2raya.git feeds/small8/luci-app-v2raya
 cd feeds/luci/themes
 rm -rf luci-theme-argon
 rm -rf luci-theme-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+git clone -b https://github.com/kenzok78/luci-theme-design.git luci-theme-design
+git clone -b https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+git clone -b https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config

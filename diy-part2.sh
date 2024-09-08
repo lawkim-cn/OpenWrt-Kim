f0f7099eb9f4c851ab6230a131c0d9ac6c40c5b8
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2024-09-08 15:42:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 # Modify default Ver
 # sed -i 's/R23.7.7/R23.7.7 Build By LawKim/g' package/lean/default-settings/files/zzz-default-settings
 
@@ -20,7 +20,6 @@ sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_gener
 # rm -rf feeds/luci/themes/luci-theme-argon
 # 拉取 argon 原作者的源码
 git clone -b https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-git clone -b https://github.com/kenzok78/luci-theme-design.git feeds/luci/themes/luci-theme-design
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci-light/Makefile
